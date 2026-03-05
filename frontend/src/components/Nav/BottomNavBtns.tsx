d0421a0ea1691e6fe7462b73abf47cc5d091cfb5
Pawel <ppazyrski@wp.pl>
2026-03-05 21:13:42 +0100

    fix: resolve active state issues in BottomNavBtns

--- frontend/src/components/Nav/BottomNavBtns.tsx
+++ frontend/src/components/Nav/BottomNavBtns.tsx
@@ -6,7 +6,13 @@ export function BottomNavBtns() {
     <div className="mt-auto flex flex-col items-center justify-end">
       <BellDot className="mb-10 h-8 w-8 cursor-pointer transition-all hover:scale-115 hover:text-(--hover-color)" />
       <NavLink to="/settings">
-        <Settings className="mb-10 h-8 w-8 cursor-pointer transition-all hover:scale-115 hover:text-(--hover-color)" />
+        {({ isActive }) => (
+          <Settings
+            className={`h-8 w-8 cursor-pointer transition-all hover:scale-110 hover:text-(--hover-color) ${
+              isActive ? "text-(--hover-color)" : "text-white"
+            }`}
+          />
+        )}
       </NavLink>
     </div>
   );
